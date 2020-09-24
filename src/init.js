f63aad41d7ed1e970331902f69b2de8ef4367837
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-09-24 02:03:41 +0200

    add shortHandler closed #8

--- src/init.js
+++ src/init.js
@@ -1,8 +1,10 @@
 'use strict';
 
-import { timeHandler } from './timeHandler.js';
+import { shortBreakHandler, timeHandler } from './timeHandler.js';
 
 console.log('hello');
 
 const startBtn = document.getElementById('start-btn');
 startBtn.addEventListener('click', timeHandler);
+const shortBreak=document.getElementById('short-break');
+shortBreak.addEventListener('click',shortBreakHandler);
--- src/timeHandler.js
+++ src/timeHandler.js
@@ -2,7 +2,20 @@ import { CountDownTimer } from './time.js';
 
 const displayCount = document.getElementById('timer-string');
 const progressBar = document.getElementById('progress-bar');
-const newCount = new CountDownTimer(1500, displayCount, progressBar);
+const pomoDoro=document.getElementById('pomodoro')
+let newCount;
+
+export const shortBreakHandler=(event)=>{
+    const shortBreak=document.getElementById('short-break');
+    const allButton=document.querySelectorAll('.time-btn-active');
+    allButton.forEach(element=>element.className='time-btn');
+    shortBreak.className='time-btn-active';
+     newCount = new CountDownTimer(300, displayCount, progressBar);
+}
+	if(pomoDoro.className='time-btn-active'){
+	 newCount = new CountDownTimer(1500, displayCount, progressBar);
+	
+};
 export const timeHandler = (event) => {
 	if (event.target.innerHTML === 'START') {
 		event.target.innerHTML = 'STOP';
